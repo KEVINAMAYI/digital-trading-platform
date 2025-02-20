656a2575256a10068fb3907139cf6c4b65ca4ffa
KEVINAMAYI <kevinamayi20@gmail.com>
2025-02-20 12:20:51 +0300

    Fix status for Auctioning...

--- resources/views/livewire/pages/auctions/view.blade.php
+++ resources/views/livewire/pages/auctions/view.blade.php
@@ -28,16 +28,16 @@ new #[Layout('layouts.app')] class extends Component {
 
             <div class="max-w-4xl mx-auto p-6">
 
-                    <!-- Submit Button -->
-                    <a wire:navigate href="{{ route('auctions.index') }}"
-                       class="mb-4 inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                        <!-- Heroicons Arrow Left -->
-                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
-                        </svg>
-                        Back
-                    </a>
+                <!-- Submit Button -->
+                <a wire:navigate href="{{ route('auctions.index') }}"
+                   class="mb-4 inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                    <!-- Heroicons Arrow Left -->
+                    <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
+                    </svg>
+                    Back
+                </a>
 
                 <div class="max-w-5xl mx-auto p-6 bg-white dark:bg-gray-800 shadow-lg rounded-lg">
                     <div class="space-y-6">
@@ -53,14 +53,6 @@ new #[Layout('layouts.app')] class extends Component {
                                 <p class="text-gray-700 dark:text-gray-300">
                                     <span class="font-semibold">End Time:</span> {{ $auction->end_time }}
                                 </p>
-                                {{--                                <p class="text-gray-700 dark:text-gray-300">--}}
-                                {{--                                    <span class="font-semibold">Volume:</span> {{ $auction->available_volume }}--}}
-                                {{--                                    <span class="text-xs font-semibold">previous volume:</span>--}}
-                                {{--                                    <span class="text-xs">--}}
-                                {{--                                        {{ $auction->available_volume }}--}}
-                                {{--                                    </span>--}}
-                                {{--                                </p>--}}
-
                                 @php
                                     $now = now();
                                     if ($auction->start_time > $now) {
@@ -81,10 +73,12 @@ new #[Layout('layouts.app')] class extends Component {
 
                             </div>
                             <div>
-                                <a wire:navigate href="{{ route('listings.create',$auction->id) }}"
-                                   class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                    Add New Listing
-                                </a>
+                                @if($status[0] !== 'Ended')
+                                    <a wire:navigate href="{{ route('listings.create',$auction->id) }}"
+                                       class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                        Add New Listing
+                                    </a>
+                                @endif
                             </div>
                         </div>
 
@@ -136,11 +130,23 @@ new #[Layout('layouts.app')] class extends Component {
                                                 </span>
                                             </td>
                                             <td class="px-6 py-4">
+                                                @if($status[0] !== 'Ended' && $status[0] !== 'Upcoming')
+                                                    <a wire:navigate href="{{ route('bids.index',$listing->id) }}"
+                                                       class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                                                        Bid
+                                                    </a>
+                                                @else
+                                                    <button disabled
+                                                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest
+                                                                      disabled:opacity-50 disabled:cursor-not-allowed
+                                                                      hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300
+                                                                      focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800
+                                                                      transition ease-in-out duration-150">
+                                                        Bid
+                                                    </button>
 
-                                                <a wire:navigate href="{{ route('bids.index',$listing->id) }}"
-                                                   class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
-                                                    Bid
-                                                </a>
+                                                @endif
+
                                             </td>
                                         </tr>
                                     @endforeach
